30186b0fde09e534543614fc87d9a02194ae3cd0
Sanek <djShtaket88@mail.ru>
2019-07-04 14:47:09 +0300

    task #1732: add 404 page

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,23 +1,68 @@
-@extends('layouts.app')
-
-@section('title', '404 - Страница не найдена')
-@section('description', '404 - Страница не найдена')
-@section('keywords', '404 - Страница не найдена')
-
-@section('content')
-
-    <section class="not__found">
-        <div class="bg__box"></div>
-        <div class="container">
-            <div class="row">
-                <div class="col-6">
-                    <div class="not__found-box">
-                        <img src="{{ asset('img/error.svg') }}" alt="страница не найдена">
-                    </div>
-                    <a href="{{ route('page.show') }}" class="btn">Перейти на главную</a>
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="wide wow-animation">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=yes"/>
+    <title>404 - Страница не найдена</title>
+    <meta name="description" content="404 - Страница не найдена">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="theme-color" content="#eee">
+    <meta name="robots" content="noindex, nofollow" />
+    <link href="{{ mix('css/app.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('favicon.ico') }}" rel="shortcut icon" type="image/x-icon" />
+</head>
+<body>
+    <div class="preloader">
+        <div class="preloader-body">
+            <div class="cssload-bell">
+                <div class="cssload-circle">
+                    <div class="cssload-inner"></div>
+                </div>
+                <div class="cssload-circle">
+                    <div class="cssload-inner"></div>
+                </div>
+                <div class="cssload-circle">
+                    <div class="cssload-inner"></div>
+                </div>
+                <div class="cssload-circle">
+                    <div class="cssload-inner"></div>
+                </div>
+                <div class="cssload-circle">
+                    <div class="cssload-inner"></div>
                 </div>
             </div>
         </div>
-    </section>
+    </div>
+    <div class="page">
+        <section class="section section-single context-dark bg-image" style="background-image: url('/images/bg-404.jpg');">
+            <div class="section-single-inner">
+                <header class="section-single-header page-header">
+                    <div class="page-head-inner">
+                        <a class="brand" href="{{ route('page.show') }}">
+                            <img class="brand-logo-light" src="{{ asset('images/logo.png') }}" alt="" />
+                        </a>
+                    </div>
+                </header>
 
-@endsection
+                <div class="section-single-main">
+                    <div class="container">
+                        <div class="title-modern">404</div>
+                        <h3 class="font-weight-light text-spacing-100">Страница не найдена</h3>
+                        <a class="button button-lg button-secondary button-zakaria" href="{{ route('page.show') }}">Вернуться на главную</a>
+                    </div>
+                </div>
+                <div class="section-single-footer">
+                    <div class="container text-center">
+                    </div>
+                </div>
+
+            </div>
+        </section>
+    </div>
+
+<div class="snackbars" id="form-output-global"></div>
+<script src="{{ asset('js/core.min.js') }}"></script>
+<script src="{{ mix('js/app.min.js') }}"></script>
+</body>
+</html>
