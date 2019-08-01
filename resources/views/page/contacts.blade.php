d9a7e017d97323be9e6c22177dfb993573391ecb
Sanek <djShtaket88@mail.ru>
2019-08-01 13:36:23 +0300

    task #1795: change form name in service page

--- resources/views/page/contacts.blade.php
+++ resources/views/page/contacts.blade.php
@@ -46,7 +46,7 @@
     @includeWhen($page->gallery, 'layouts.sections.gallery', ['gallery' => $page->gallery])
 
     <section class="section">
-        <div class="yandex-map-container">
+        <div>
             <iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3Ac4db9bb6e87fa6d628253543b41ef61b3a8bde8c8d8cd63bca063905416cf583&amp;source=constructor" width="100%" height="450" frameborder="0"></iframe>
         </div>
     </section>
