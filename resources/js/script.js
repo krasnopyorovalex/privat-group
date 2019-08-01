3914b6844b781cd61c61310f2173335a38bc3b2f
Sanek <djShtaket88@mail.ru>
2019-08-01 10:20:06 +0300

    optimize js

--- resources/js/script.js
+++ resources/js/script.js
@@ -52,7 +52,7 @@
             pageTransition({
                 target: document.querySelector('.page'),
                 delay: 0,
-                duration: 500,
+                duration: 350,
                 classIn: 'fadeIn',
                 classOut: 'fadeOut',
                 classActive: 'animated',
