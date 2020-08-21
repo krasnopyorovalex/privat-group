45f656a9af0f92cea097c025e60e73c9f06690d9
a.krasnopyorov <a.krasnoperov@easy-sms.ru>
2020-08-21 14:16:10 +0300

    add btn to slider with link

--- resources/views/layouts/sections/slider.blade.php
+++ resources/views/layouts/sections/slider.blade.php
@@ -2,26 +2,37 @@
 <section class="section swiper-container swiper-slider swiper-slider-1" data-autoplay="5000">
     <div class="swiper-wrapper context-dark text-center">
         @foreach ($slider->images as $image)
-        <div class="swiper-slide darken" data-slide-bg="{{ asset($image->getPath()) }}">
-            <div class="swiper-slide-caption section-md">
-                <div class="container">
-                    @if($image->name)
-                    <div class="swiper-title-2" data-caption-animate="fadeInLeft" data-caption-delay="200">{{ $image->name }}</div>
-                    @endif
-                    @if($image->alt)
-                    <div class="swiper-title-2" data-caption-animate="fadeScale" data-caption-delay="100">{{ $image->alt }}</div>
-                    @endif
-                    @if($image->title)
-                    <h2 class="swiper-title-2" data-caption-animate="fadeInRight" data-caption-delay="200">{{ $image->title }}</h2>
-                    @endif
+            <div class="swiper-slide darken" data-slide-bg="{{ asset($image->getPath()) }}">
+                <div class="swiper-slide-caption section-md">
+                    <div class="container">
+                        @if($image->name)
+                            <div class="swiper-title-2" data-caption-animate="fadeInLeft"
+                                 data-caption-delay="200">{{ $image->name }}</div>
+                        @endif
+                        @if($image->alt)
+                            <div class="swiper-title-2" data-caption-animate="fadeScale"
+                                 data-caption-delay="100">{{ $image->alt }}</div>
+                        @endif
+                        @if($image->title)
+                            <h2 class="swiper-title-2" data-caption-animate="fadeInRight"
+                                data-caption-delay="200">{{ $image->title }}</h2>
+                        @endif
+                        @if($image->link)
+                            <div class="button-wrap" data-caption-animate="fadeInUp" data-caption-delay="300">
+                                <a class="button button-lg button-shadow-3 button-secondary button-zakaria"
+                                   data-toggle="modal" data-target="#callback" href="{{ $image->link }}">
+                                    Подробнее
+                                </a>
+                            </div>
+                        @endif
+                    </div>
                 </div>
             </div>
-        </div>
         @endforeach
     </div>
-{{--    <!-- Swiper Pagination-->--}}
-{{--    <div class="swiper-pagination"></div>--}}
-{{--    <!-- Swiper Navigation-->--}}
-{{--    <div class="swiper-button-prev"></div>--}}
-{{--    <div class="swiper-button-next"></div>--}}
+    {{--    <!-- Swiper Pagination-->--}}
+    {{--    <div class="swiper-pagination"></div>--}}
+    {{--    <!-- Swiper Navigation-->--}}
+    {{--    <div class="swiper-button-prev"></div>--}}
+    {{--    <div class="swiper-button-next"></div>--}}
 </section>
