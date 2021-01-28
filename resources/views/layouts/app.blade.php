c27f69336015c7256fba9d38348bdedcb5a667d6
Александр Краснопёров <krasnopyorovalex88@gmail.com>
2021-01-28 09:38:23 +0300

    update layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -177,7 +177,7 @@
                                         <div class="unit-left"><span class="icon mdi mdi-phone"></span></div>
                                         <div class="unit-body">
                                             <a href="tel:+79789455747">+7 (978) 94-557-47</a>
-                                            <span class="h-icons-list mobile_phone">
+                                            <span class="h-icons-list">
                                                 <a href="https://wa.me/79789455747" rel="noopener noreferrer"><img src="{{ asset('images/viber.svg') }}" alt=""></a>
                                                 <a href="viber://add?number=79789455747" rel="noopener noreferrer"><img src="{{ asset('images/whatsapp.svg') }}" alt=""></a>
                                             </span>
